a8ee4a7e948272224fb8c805193e2f06895b55f3
Pasha Peryakin <pashaandsik@gmail.com>
2016-05-30 08:55:16 +0300

    tmp

---
+++ vendor/admin/admin.gemspec
@@ -0,0 +1,23 @@
+$:.push File.expand_path("../lib", __FILE__)
+
+# Maintain your gem's version:
+require "admin/version"
+
+# Describe your gem and declare its dependencies:
+Gem::Specification.new do |s|
+  s.name        = "admin"
+  s.version     = Admin::VERSION
+  s.authors     = ["Pasha Peryakin"]
+  s.email       = ["pashaandsik@gmail.com"]
+  s.homepage    = ""
+  s.summary     = "Admin."
+  s.description = "Admin."
+  s.license     = "MIT"
+
+  s.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.rdoc"]
+  s.test_files = Dir["test/**/*"]
+
+  s.add_dependency "rails", "~> 4.2.6"
+
+  s.add_development_dependency "sqlite3"
+end
---
+++ vendor/admin/app/controllers/admin/application_controller.rb
@@ -0,0 +1,54 @@
+module Admin
+  class ApplicationController < ActionController::Base
+    before_action :check_ip
+    before_action :require_user
+    before_action :check_access
+    before_action :set_locale
+
+    delegate :current_user, :sign_in, :sign_out, :guest?, to: :user_session
+    helper_method :current_user
+
+    def user_session
+      @user_session ||= AdminUserSession.new(self)
+    end
+
+    private
+
+    def page
+      (params[:page] || 1).to_i
+    end
+
+    def check_ip
+      ips =  %w[127.0.0.1]
+      raise "NotAllowed" unless ips.include?(request.remote_ip)
+    end
+
+    def check_access
+      return true unless current_user
+      # authorize!(:view, :basic_info)
+    end
+
+    def require_guest
+      redirect_to :root and return if current_user
+    end
+
+    def require_user
+      redirect_to :login and return unless current_user
+    end
+
+    # def ability
+    #   @ability ||= Crm::Ability.new(current_user)
+    # end
+    #
+    # delegate :can?, :cannot?, :authorize!, :editable_fields, to: :ability
+    # helper_method :can?, :cannot?
+
+    def redirect_to_index(*args)
+      redirect_to({ action: :index }, *args)
+    end
+
+    def set_locale
+      I18n.locale = :en
+    end
+  end
+end
